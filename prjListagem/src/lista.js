b8e64494ea8de57ea20f91b1d26106b1bea3f540
ivanclay <ivan.clay.moura@gmail.com>
2021-08-24 15:22:20 -0300

    add codigo

---
+++ prjListagem/src/lista.js
@@ -0,0 +1,14 @@
+import uuid from 'uuid/v4';
+
+export default [
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 01', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 02', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 03', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 04', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 05', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 06', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 07', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 08', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 09', done: false},
+  {id: uuid(), task: 'Ivan Estudar React Native MOD 10', done: false},
+];
