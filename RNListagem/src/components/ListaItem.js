4b2b5be10fa83bbf32456bb6aef2aec068ba70bf
ivanclay <ivan.clay.moura@gmail.com>
2021-08-25 09:02:16 -0300

    MD7 :: Marcando como feito.

--- RNListagem/src/components/ListaItem.js
+++ RNListagem/src/components/ListaItem.js
@@ -8,25 +8,26 @@ const Item = styled.TouchableOpacity`
   flex-direction: row;
 `;
 
-const ItemText = styled.Text`
-  font-size: 15px;
-  flex: 1;
-`;
-
 const ItemCheck = styled.View`
   width: 20px;
   height: 20px;
   border-radius: 10px;
   border: 3px solid #000;
-  background-color: #fff;
+  background-color: ${props=>props.done ? '#ff0' : '#fff'};
+`;
+
+const ItemText = styled.Text`
+  flex: 1;
+  font-size: 15px;
+  text-decoration: ${props=>props.done ? 'line-through' : ''};
 `;
 
 export default (props) => {
   return (
-    <Item onPress={() => {}} activeOpacity={0.4}>
+    <Item onPress={props.onPress} activeOpacity={0.4}>
       <>
         <ItemText>{props.data.task}</ItemText>
-        <ItemCheck />
+        <ItemCheck done={props.data.done} />
       </>
     </Item>
   );
